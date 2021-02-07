fc688ba66cfeb706e94344e95b96870b97ddee00
yf L <lyf571321556@gmail.com>
2021-02-07 18:57:38 +0800

    add: gitaction构建迁移尝试step7

--- ios/fastlane/build_beta.sh
+++ ios/fastlane/build_beta.sh
@@ -1,7 +1,7 @@
 echo "===Building iOS Beta==="
 pwd
 flutter build --verbose ios --no-tree-shake-icons --release --no-codesign --flavor Beta -t lib/main_dev.dart --dart-define=username=wuxingjuan@ones.ai --dart-define=password=juan1997 --dart-define=isdev=true --dart-define=projectServerUrl=https://devapi.myones.net/project/F8032 --dart-define=wikiServerUrl=https://dev.myones.net/wiki/F8032
-cd $MOBILE_ROOT/ios
+cd $GITHUB_WORKSPACE/ios
 pwd
 bundle exec pod install --repo-update
 bundle exec fastlane beta --verbose
