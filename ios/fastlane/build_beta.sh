7e12bb89d2dbf421f503a1b965df9687b6fe1109
yf L <lyf571321556@gmail.com>
2021-02-08 15:28:52 +0800

    add: 自定义xcode workspace的scheme以及对应的configuration，解决The Xcode project does not define custom schemes. You cannot use the --flavor option.

--- ios/fastlane/build_beta.sh
+++ ios/fastlane/build_beta.sh
@@ -1,5 +1,6 @@
 echo "===Building iOS Beta==="
 pwd
+#构建flutter代码和资源，必须自定义一个new_scheme,以及对应的build configuration（Release-new_scheme_name）,不然会报错(The Xcode project does not define custom schemes. You cannot use the --flavor option.)
 flutter build --verbose ios --no-tree-shake-icons --release --no-codesign --flavor Beta -t lib/main.dart --dart-define=username=wuxingjuan@ones.ai --dart-define=password=juan1997 --dart-define=isdev=true --dart-define=projectServerUrl=https://devapi.myones.net/project/F8032 --dart-define=wikiServerUrl=https://dev.myones.net/wiki/F8032
 cd $GITHUB_WORKSPACE/ios
 pwd
--- ios/fastlane/build_release.sh
+++ ios/fastlane/build_release.sh
@@ -1,5 +1,6 @@
 echo "===Building iOS Release==="
 pwd
+#构建flutter代码和资源，必须自定义一个new_scheme,以及对应的build configuration（Release-new_scheme_name）,不然会报错(The Xcode project does not define custom schemes. You cannot use the --flavor option.)
 flutter build ios --no-tree-shake-icons --release --no-codesign --flavor Beta -t lib/main.dart
 cd $GITHUB_WORKSPACE/ios
 pwd
