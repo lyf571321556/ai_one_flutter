ad02929b1f283f1dc233c123a47cbee8b79c0e8a
yf L <lyf571321556@gmail.com>
2021-02-09 18:13:22 +0800

    add: gitaction android构建迁移尝试step9

---
+++ android/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+echo "build wordspace:$GITHUB_WORKSPACE"
+echo "CURRENT_TAG:$CURRENT_TAG"
+echo "CURRENT_BRANCH:$CURRENT_BRANCH"
+flutter doctor
+flutter pub get
+
+if [[ "${CURRENT_TAG}" =~ v[0-9]+.[0-9]+.[0-9]+ ]]; then
+  echo "===Building Android Release==="
+  flutter build apk --no-tree-shake-icons --release --flavor production -t lib/main.dart
+else [[ "${TRAVIS_PULL_REQUEST}" =~ F[0-9]+ ]]
+  echo "===Building Android Beta==="
+  flutter build apk --verbose --no-tree-shake-icons --release --flavor preview -t lib/main_dev.dart --dart-define=username=wuxingjuan@ones.ai --dart-define=password=juan1997 --dart-define=isdev=true --dart-define=projectServerUrl=https://devapi.myones.net/project/F8032 --dart-define=wikiServerUrl=https://dev.myones.net/wiki/F8032
+fi
+exit 0
