e6df74c971bc63849521c5682efbacdd9f538a26
yf L <lyf571321556@gmail.com>
2021-02-10 14:38:15 +0800

    add: gitaction android构建迁移尝试step22

--- web/build.sh
+++ web/build.sh
@@ -11,7 +11,9 @@ mark_last_build_revision() {
     echo "$BUILD_REVISION" > "$LAST_BUILD_REVISION_FILE"
 }
 
-WEB_OUTPUT_FILE="$GITHUB_WORKSPACE/ones-ai-mobile-web-$TRAVIS_TAG.tar.gz"
+git fetch --unshallow
+BUILD_REVISION=`git rev-list HEAD --count`
+
 
 pkg_to_tarfile() {
     pwd
@@ -34,17 +36,16 @@ build_mobile_web() {
     echo "finish build..."
 }
 
-git fetch --unshallow
-BUILD_REVISION=`git rev-list HEAD --count`
-
 build_mobile_web
 pkg_to_tarfile
 
 if [[ "${CURRENT_TAG}" =~ v[0-9]+.[0-9]+.[0-9]+ ]]; then
+    WEB_OUTPUT_FILE="$GITHUB_WORKSPACE/ones-ai-mobile-web-$TRAVIS_TAG-$BUILD_REVISION.tar.gz"
     echo "start upload artifact to tag $CURRENT_TAG"
     echo "upload package $WEB_OUTPUT_FILE ......"
     echo "finish upload artifact to tag $CURRENT_TAG"
 elif [[ "${CURRENT_BRANCH}" =~ F[0-9]+ ]]; then
+    WEB_OUTPUT_FILE="$GITHUB_WORKSPACE/ones-ai-mobile-web-$CURRENT_BRANCH-$BUILD_REVISION.tar.gz"
     echo "ignore artifact for branch $CURRENT_BRANCH"
 fi
 exit 0
