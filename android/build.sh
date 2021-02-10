2d7777dbc09293f416625e1d1f7eeaee4c033c65
yf L <lyf571321556@gmail.com>
2021-02-10 22:50:21 +0800

    add: gitaction android构建迁移尝试step30

--- android/build.sh
+++ android/build.sh
@@ -9,10 +9,10 @@ flutter pub get
 if [[ "${CURRENT_TAG}" =~ v[0-9]+.[0-9]+.[0-9]+ ]]; then
     echo "===Building Android Release==="
     flutter build apk --no-tree-shake-icons --release --flavor production -t lib/main.dart
-    cp "./build/app/outputs/flutter-apk/app-production-release.apk" "$GITHUB_WORKSPACE/ones-mobile-$CURRENT_TAG.apk"
+    cp "./build/app/outputs/flutter-apk/app-production-release.apk" "$GITHUB_WORKSPACE/ones-ai-mobile-$CURRENT_TAG.apk"
 elif [[ "${CURRENT_BRANCH}" =~ F[0-9]+ ]]; then
     echo "===Building Android Beta==="
     flutter build apk --verbose --no-tree-shake-icons --release --flavor preview -t lib/main.dart --dart-define=username=wuxingjuan@ones.ai --dart-define=password=juan1997 --dart-define=isdev=true --dart-define=projectServerUrl=https://devapi.myones.net/project/F8032 --dart-define=wikiServerUrl=https://dev.myones.net/wiki/F8032
-    cp "./build/app/outputs/apk/preview/release/app-preview-release.apk"  "$GITHUB_WORKSPACE/ones-mobile-$CURRENT_BRANCH.apk"
+    cp "./build/app/outputs/apk/preview/release/app-preview-release.apk"  "$GITHUB_WORKSPACE/ones-ai-mobile-$CURRENT_BRANCH.apk"
 fi
 exit 0
