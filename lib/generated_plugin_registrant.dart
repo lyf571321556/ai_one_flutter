1d765d5dfb50dbe7850790dabaab293914897263
yf L <lyf571321556@gmail.com>
2021-02-21 01:15:37 +0800

    add: 三端gitaction打包验证通过.

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -2,7 +2,8 @@
 // Generated file. Do not edit.
 //
 
-// ignore_for_file: lines_longer_than_80_chars
+// ignore: unused_import
+import 'dart:ui';
 
 import 'package:connectivity_for_web/connectivity_for_web.dart';
 import 'package:flutter_keyboard_visibility_web/flutter_keyboard_visibility_web.dart';
@@ -12,10 +13,10 @@ import 'package:url_launcher_web/url_launcher_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
-void registerPlugins(Registrar registrar) {
-  ConnectivityPlugin.registerWith(registrar);
-  FlutterKeyboardVisibilityPlugin.registerWith(registrar);
-  SharedPreferencesPlugin.registerWith(registrar);
-  UrlLauncherPlugin.registerWith(registrar);
-  registrar.registerMessageHandler();
+void registerPlugins(PluginRegistry registry) {
+  ConnectivityPlugin.registerWith(registry.registrarFor(ConnectivityPlugin));
+  FlutterKeyboardVisibilityPlugin.registerWith(registry.registrarFor(FlutterKeyboardVisibilityPlugin));
+  SharedPreferencesPlugin.registerWith(registry.registrarFor(SharedPreferencesPlugin));
+  UrlLauncherPlugin.registerWith(registry.registrarFor(UrlLauncherPlugin));
+  registry.registerMessageHandler();
 }
