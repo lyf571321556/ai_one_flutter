d91e0ba326fec5e0eb670eb1d40d0945123baa13
yf L <lyf571321556@gmail.com>
2021-02-22 17:03:04 +0800

    add: android,ios构建步骤统一放在fastlane文件中!

--- android/build.sh
+++ android/build.sh
@@ -4,16 +4,18 @@ set -e
 echo "Build Env Info:Build FLUTTER_VERSION:$FLUTTER_VERSION,Build FLUTTER_CHANNEL:$FLUTTER_CHANNEL"
 echo "Build Params Info:Build Dir:$GITHUB_WORKSPACE,Build Tag:$CURRENT_TAG,Build Branch:$CURRENT_BRANCH,Build BUILD_REVISION:$BUILD_REVISION"
 bundle install --gemfile android/Gemfile
-flutter pub get
+cd $GITHUB_WORKSPACE/android
 
 if [[ "${CURRENT_TAG}" =~ v[0-9]+.[0-9]+.[0-9]+ ]]; then
     echo "===Building Android Release==="
-    flutter build apk --no-tree-shake-icons --release --flavor production -t lib/main.dart
-    cp "./build/app/outputs/flutter-apk/app-production-release.apk" "$GITHUB_WORKSPACE/ones-ai-mobile-$CURRENT_TAG.apk"
+#    flutter build apk --no-tree-shake-icons --release --flavor production -t lib/main.dart
+#    cp "./build/app/outputs/flutter-apk/app-production-release.apk" "$GITHUB_WORKSPACE/ones-ai-mobile-$CURRENT_TAG.apk"
+    bundle exec fastlane release --verbose
 elif [[ "${CURRENT_BRANCH}" =~ F[0-9]+ ]]; then
     echo "===Building Android Beta==="
 #    flutter build apk --verbose --no-tree-shake-icons --release --flavor preview -t lib/main.dart --dart-define=username=wuxingjuan@ones.ai --dart-define=password=juan1997 --dart-define=isdev=true --dart-define=projectServerUrl=https://devapi.myones.net/project/F8032 --dart-define=wikiServerUrl=https://dev.myones.net/wiki/F8032
 #    cp "./build/app/outputs/apk/preview/release/app-preview-release.apk"  "$GITHUB_WORKSPACE/ones-ai-mobile-$CURRENT_BRANCH.apk"
-    sh $GITHUB_WORKSPACE/android/fastlane/build_beta.sh
+#    sh $GITHUB_WORKSPACE/android/fastlane/build_beta.sh
+    bundle exec fastlane beta --verbose
 fi
 exit 0
--- ios/build.sh
+++ ios/build.sh
@@ -2,18 +2,21 @@ set -e
 echo "Build Env Info:Build FLUTTER_VERSION:$FLUTTER_VERSION,Build FLUTTER_CHANNEL:$FLUTTER_CHANNEL"
 echo "Build Params Info:Build Dir:$GITHUB_WORKSPACE,Build Tag:$CURRENT_TAG,Build Branch:$CURRENT_BRANCH,Build BUILD_REVISION:$BUILD_REVISION"
 
-bundle install --gemfile $GITHUB_WORKSPACE/ios/Gemfile
-flutter pub get
+bundle install --gemfile ios/Gemfile
+cd $GITHUB_WORKSPACE/ios
+bundle exec pod install --repo-update
 
 rm -rf ~/Library/MobileDevice/Provisioning\ Profiles*
 
 if [[ "${CURRENT_TAG}" =~ v[0-9]+.[0-9]+.[0-9]+ ]]; then
     echo "===Building iOS Release==="
-    sh $GITHUB_WORKSPACE/ios/fastlane/build_release.sh
-    cp "$GITHUB_WORKSPACE/ios/fastlane/ipas/ones_mobile.ipa" "$GITHUB_WORKSPACE/ones-ai-mobile-$CURRENT_TAG.ipa"
+#    sh $GITHUB_WORKSPACE/ios/fastlane/build_release.sh
+#    cp "$GITHUB_WORKSPACE/ios/fastlane/ipas/ones_mobile.ipa" "$GITHUB_WORKSPACE/ones-ai-mobile-$CURRENT_TAG.ipa"
+    bundle exec fastlane release --verbose
 elif [[ "${CURRENT_BRANCH}" =~ F[0-9]+ ]]; then
     echo "===Building iOS Beta==="
-    sh $GITHUB_WORKSPACE/ios/fastlane/build_beta.sh
-    cp "$GITHUB_WORKSPACE/ios/fastlane/ipas/ones_mobile.ipa"  "$GITHUB_WORKSPACE/ones-ai-mobile-$CURRENT_BRANCH.ipa"
+#    sh $GITHUB_WORKSPACE/ios/fastlane/build_beta.sh
+#    cp "$GITHUB_WORKSPACE/ios/fastlane/ipas/ones_mobile.ipa"  "$GITHUB_WORKSPACE/ones-ai-mobile-$CURRENT_BRANCH.ipa"
+    bundle exec fastlane beta --verbose
 fi
 exit 0
